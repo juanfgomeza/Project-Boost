af429ca9c9aee6c6ee433b5de70c116f318baaa2
AzureAD\JuanFernandoGomez <juanfgomeza@gmail.com>
2022-03-20 00:02:04 -0500

    Up to lesson 55

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,20 +4,98 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay =1f;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip successSound;
+
+    [SerializeField] ParticleSystem crashParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    [SerializeField] bool collisionsEnabled = true;
+
+    AudioSource audioSource;
+
+    bool isTransiotioning = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        CheatKeys();
+    }
+
+
     void OnCollisionEnter(Collision other) {
+        if (isTransiotioning || !collisionsEnabled) return;
+        
         switch (other.gameObject.tag)
         {
             case "Friendly":
                 Debug.Log("Hit a friendly object");
                 break;
             case "Finish":
-                Debug.Log("Heyyy you did it!");
+                Debug.Log("Level completed!");
+                StartNextLevelSequence();
                 break;
             default:
-                ReloadLevel();                
+                StartCrashSequence();                
                 break;
         }
         
+    }
+
+    void CheatKeys()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            NextLevel();
+
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            ToggleDisableCollisions();
+        }
+    }
+    void ToggleDisableCollisions()
+    {
+        collisionsEnabled = !collisionsEnabled;
+    }
+
+    void StartNextLevelSequence()
+    {
+        isTransiotioning = true;
+        GetComponent<Movement>().enabled = false;
+        audioSource.Stop();
+        audioSource.PlayOneShot(successSound);
+        successParticles.Play();
+        Invoke("NextLevel",levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        isTransiotioning = true;
+        GetComponent<Movement>().enabled = false;
+        audioSource.Stop();
+        audioSource.PlayOneShot(crashSound);        
+        crashParticles.Play();
+        Invoke("ReloadLevel",levelLoadDelay);
+
+        
+        
+    }
+
+    void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
     void ReloadLevel()
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,56 +1,112 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Rigidbody rb;
-    AudioSource audioSource;
+    // PARAMETERS - for tuning in the editor
+    // CACHE - references for readability or speed
+    // STATE - private instance (member) variables
+    
     [SerializeField] float mainThrust = 1f;
     [SerializeField] float rotationThrust = 1f;
+    [SerializeField] AudioClip mainEngine;
+
+    [SerializeField] ParticleSystem rightBoosterParticles;
+    [SerializeField] ParticleSystem leftBoosterParticles;
+    [SerializeField] ParticleSystem mainBoosterParticles;
+
+    
+    Rigidbody rb;
+    AudioSource audioSource;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         ProccessThrust();
-        ProcessRotation();        
+        ProcessRotation();            
     }
 
     void ProccessThrust()
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            //Debug.Log("Pressed SPACE - Thrusting");           
-            rb.AddRelativeForce(Time.deltaTime * mainThrust * Vector3.up);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-            
-        } 
+            StartThrusting();
+        }
         else
         {
-            audioSource.Stop();
+            StopThrusting();
         }
     }
+
     void ProcessRotation()
     {
         if(Input.GetKey(KeyCode.D))
         {
-            //Debug.Log("Pressed D - Move right");
-            ApplyRotation(rotationThrust);
+            RotateRight();
+
         }
         else if(Input.GetKey(KeyCode.A))
         {
-            //Debug.Log("Pressed A - Move left");
-            ApplyRotation(-rotationThrust);
+            RotateLeft();
+
         }
+        else
+        {
+            StopRotating();
+        }
+
+    }
+
+
+    void StartThrusting()
+    {
+        rb.AddRelativeForce(Time.deltaTime * mainThrust * Vector3.up);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        if (!mainBoosterParticles.isPlaying)
+        {
+            mainBoosterParticles.Play();
+        }
+    }
+    
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        mainBoosterParticles.Stop();
+    }
+    
+    void RotateRight()
+    {
+        ApplyRotation(rotationThrust);
+        if (!leftBoosterParticles.isPlaying)
+        {
+            leftBoosterParticles.Play();
+        }
+    }
+
+    void RotateLeft()
+    {
+        ApplyRotation(-rotationThrust);
+        if (!rightBoosterParticles.isPlaying)
+        {
+            rightBoosterParticles.Play();
+        }
+    }
+
+    void StopRotating()
+    {
+        rightBoosterParticles.Stop();
+        leftBoosterParticles.Stop();
     }
 
     void ApplyRotation(float rotationThisFrame)
