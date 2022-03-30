b64ff220b94e9eaa2d1e982816c162fce4484473
AzureAD\JuanFernandoGomez <juanfgomeza@gmail.com>
2022-03-29 22:41:22 -0500

    Oscillator

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    Vector3 finalPosition;
+    [SerializeField] Vector3 movementVector;
+    //[SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+        Debug.Log(startingPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;  // continually growing over time
+        const float tau = Mathf.PI * 2; // constant value of 2*PI
+        float rawSinWave = Mathf.Sin(cycles * tau);  // from  -1 to 1
+        
+        movementFactor = rawSinWave;
+
+        Vector3 offset = movementVector * movementFactor;
+        finalPosition = startingPosition + offset;
+        transform.position = finalPosition;
+        
+
+
+    }
+}
